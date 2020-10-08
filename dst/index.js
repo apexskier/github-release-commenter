f78fee52ebce10d6c60d7b867e9fa8a5c493c148
Cameron Little <cameron@camlittle.com>
2020-10-08 21:17:33 +0200

    Fix checks, again

--- dst/index.js
+++ dst/index.js
@@ -3,9 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core = require("@actions/core");
 var github = require("@actions/github");
 try {
-  // Get the JSON webhook payload for the event that triggered the workflow
-  var payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log("The event payload: " + payload);
-} catch (error) {
-  core.setFailed(error.message);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    var payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log("The event payload: " + payload);
+}
+catch (error) {
+    core.setFailed(error.message);
 }

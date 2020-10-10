792c48dc34e58c25874bbb61f547e52eca16f324
Cameron Little <cameron@camlittle.com>
2020-10-10 19:12:14 +0200

    Restructure tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,20 +1,7 @@
 import type * as githubModule from "@actions/github";
+import type * as coreModule from "@actions/core";
 
-jest.mock("@actions/core", () => ({
-  getInput: jest.fn((key) => {
-    if (key == "GITHUB_TOKEN") {
-      return "GITHUB_TOKEN_VALUE";
-    }
-    if (key == "comment-template") {
-      return "Included in release {release_link}";
-    }
-    fail(`Unexpected input key ${key}`);
-  }),
-  info: jest.fn(),
-  warning: console.warn.bind(console),
-  error: console.error.bind(console),
-}));
-
+jest.mock("@actions/core");
 jest.mock("@actions/github");
 
 type Mocked<T> = {
@@ -23,8 +10,50 @@ type Mocked<T> = {
     : jest.Mocked<Partial<T[P]>>;
 };
 
-test("main test", async () => {
-  const mockOctokit = {
+const github = require("@actions/github") as jest.Mocked<
+  Mocked<typeof githubModule>
+>;
+const core = require("@actions/core") as jest.Mocked<Mocked<typeof coreModule>>;
+
+describe("tests", () => {
+  let mockOctokit: any = {};
+  (github.context as any) = {
+    payload: {
+      repo: {
+        owner: "owner",
+        repo: "repo",
+      },
+      repository: { html_url: "http://repository" },
+    },
+  };
+
+  (core.warning as any) = jest.fn(console.warn.bind(console));
+  (core.error as any) = jest.fn(console.error.bind(console));
+
+  const commentTempate = "Included in release {release_link}";
+
+  beforeEach(() => {
+    github.getOctokit.mockReset().mockImplementationOnce(((token: string) => {
+      expect(token).toBe("GITHUB_TOKEN_VALUE");
+      return mockOctokit;
+    }) as any);
+
+    (core.getInput as any).mockImplementation((key: string) => {
+      if (key == "GITHUB_TOKEN") {
+        return "GITHUB_TOKEN_VALUE";
+      }
+      if (key == "comment-template") {
+        return commentTempate;
+      }
+      fail(`Unexpected input key ${key}`);
+    });
+  });
+  //
+  // afterEach(() => {
+  //   delete require.cache[require.resolve("./index")];
+  // })
+
+  const simpleMockOctokit = {
     repos: {
       listReleases: jest.fn(() =>
         Promise.resolve({
@@ -42,53 +71,19 @@ test("main test", async () => {
       ),
       compareCommits: jest.fn(() =>
         Promise.resolve({
-          data: { commits: [{ sha: "SHA1" }, { sha: "SHA2" }] },
+          data: { commits: [{ sha: "SHA1" }] },
         })
       ),
     },
     graphql: jest.fn(() =>
       Promise.resolve({
         resource: {
-          messageHeadlineHTML:
-            '<span class="issue-keyword tooltipped tooltipped-se" aria-label="This commit closes issue #3.">Closes</span> <a class="issue-link js-issue-link" data-error-text="Failed to load title" data-id="718013420" data-permission-text="Title is private" data-url="https://github.com/apexskier/github-release-commenter/issues/1" data-hovercard-type="issue" data-hovercard-url="/apexskier/github-release-commenter/issues/1/hovercard" href="https://github.com/apexskier/github-release-commenter/issues/1">#1</a>',
+          messageHeadlineHTML: "",
           messageBodyHTML:
             '<span class="issue-keyword tooltipped tooltipped-se" aria-label="This commit closes issue #123.">Closes</span> <p><span class="issue-keyword tooltipped tooltipped-se" aria-label="This pull request closes issue #7.">Closes</span>',
           associatedPullRequests: {
             pageInfo: { hasNextPage: false },
-            edges: [
-              {
-                node: {
-                  bodyHTML:
-                    '<span class="issue-keyword tooltipped tooltipped-se" aria-label="This commit closes issue #4.">Closes</span> <span class="issue-keyword tooltipped tooltipped-se" aria-label="This commit closes issue #5.">Closes</span>',
-                  number: 9,
-                  timelineItems: {
-                    pageInfo: { hasNextPage: false },
-                    nodes: [
-                      {
-                        isCrossRepository: true,
-                        __typename: "ConnectedEvent",
-                        subject: { number: 1 },
-                      },
-                      {
-                        isCrossRepository: false,
-                        __typename: "ConnectedEvent",
-                        subject: { number: 2 },
-                      },
-                      {
-                        isCrossRepository: false,
-                        __typename: "DisconnectedEvent",
-                        subject: { number: 2 },
-                      },
-                      {
-                        isCrossRepository: false,
-                        __typename: "ConnectedEvent",
-                        subject: { number: 2 },
-                      },
-                    ],
-                  },
-                },
-              },
-            ],
+            edges: [],
           },
         },
       })
@@ -97,26 +92,94 @@ test("main test", async () => {
       createComment: jest.fn(() => Promise.resolve()),
     },
   };
-  const github = require("@actions/github") as jest.Mocked<
-    Mocked<typeof githubModule>
-  >;
-  (github.context as any) = {
-    payload: {
-      repo: {
-        owner: "owner",
-        repo: "repo",
+
+  test("main test", async () => {
+    mockOctokit = {
+      repos: {
+        listReleases: jest.fn(() =>
+          Promise.resolve({
+            data: [
+              {
+                tag_name: "current_tag_name",
+                html_url: "http://current_release",
+              },
+              {
+                tag_name: "prior_tag_name",
+                html_url: "http://prior_release",
+              },
+            ],
+          })
+        ),
+        compareCommits: jest.fn(() =>
+          Promise.resolve({
+            data: { commits: [{ sha: "SHA1" }, { sha: "SHA2" }] },
+          })
+        ),
       },
-      repository: { html_url: "http://repository" },
-    },
-  };
-  github.getOctokit.mockImplementationOnce(((token: string) => {
-    expect(token).toBe("GITHUB_TOKEN_VALUE");
-    return mockOctokit;
-  }) as any);
+      graphql: jest.fn(() =>
+        Promise.resolve({
+          resource: {
+            messageHeadlineHTML:
+              '<span class="issue-keyword tooltipped tooltipped-se" aria-label="This commit closes issue #3.">Closes</span> <a class="issue-link js-issue-link" data-error-text="Failed to load title" data-id="718013420" data-permission-text="Title is private" data-url="https://github.com/apexskier/github-release-commenter/issues/1" data-hovercard-type="issue" data-hovercard-url="/apexskier/github-release-commenter/issues/1/hovercard" href="https://github.com/apexskier/github-release-commenter/issues/1">#1</a>',
+            messageBodyHTML:
+              '<span class="issue-keyword tooltipped tooltipped-se" aria-label="This commit closes issue #123.">Closes</span> <p><span class="issue-keyword tooltipped tooltipped-se" aria-label="This pull request closes issue #7.">Closes</span>',
+            associatedPullRequests: {
+              pageInfo: { hasNextPage: false },
+              edges: [
+                {
+                  node: {
+                    bodyHTML:
+                      '<span class="issue-keyword tooltipped tooltipped-se" aria-label="This commit closes issue #4.">Closes</span> <span class="issue-keyword tooltipped tooltipped-se" aria-label="This commit closes issue #5.">Closes</span>',
+                    number: 9,
+                    timelineItems: {
+                      pageInfo: { hasNextPage: false },
+                      nodes: [
+                        {
+                          isCrossRepository: true,
+                          __typename: "ConnectedEvent",
+                          subject: { number: 1 },
+                        },
+                        {
+                          isCrossRepository: false,
+                          __typename: "ConnectedEvent",
+                          subject: { number: 2 },
+                        },
+                        {
+                          isCrossRepository: false,
+                          __typename: "DisconnectedEvent",
+                          subject: { number: 2 },
+                        },
+                        {
+                          isCrossRepository: false,
+                          __typename: "ConnectedEvent",
+                          subject: { number: 2 },
+                        },
+                      ],
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        })
+      ),
+      issues: {
+        createComment: jest.fn(() => Promise.resolve()),
+      },
+    };
 
-  require("./index");
+    jest.isolateModules(() => {
+      require("./index");
+    });
 
-  await new Promise((resolve) => setImmediate(() => resolve()));
+    await new Promise((resolve) => setImmediate(() => resolve()));
 
-  expect(mockOctokit).toMatchSnapshot();
+    expect(mockOctokit).toMatchSnapshot();
+  });
+
+  describe("feature tests", () => {
+    beforeEach(() => {
+      mockOctokit = simpleMockOctokit;
+    });
+  });
 });
